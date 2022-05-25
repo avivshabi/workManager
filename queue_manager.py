16684d38e0aba8070ad0c782c02378faaf6e5545
Aviv Shabi <62796670+avivshabi@users.noreply.github.com>
2022-05-25 08:37:10 +0300

    refactor: improved imports

--- queue_manager.py
+++ queue_manager.py
@@ -1,18 +1,18 @@
 import os
 from redis import Redis
-from rq import Queue, registry, job, Connection
+from rq import Queue, job, Connection, Worker
 from worker import work
 
 
 class QueueManager:
     def __init__(self):
         self._conn = Redis(
-            host=os.getenv('REDIS_HOST', '127.0.0.1'),
+            host=os.getenv('REDIS_HOST', '0.0.0.0'),
             port=os.getenv('REDIS_PORT', 6379),
-            password=os.getenv('REDIS_PASSWORD', '')
+            password=os.getenv('REDIS_PASSWORD', ''),
         )
         self._queue = Queue(connection=self._conn)
-        self._completed = registry.FinishedJobRegistry(connection=self._conn)
+        self._completed = self._queue.finished_job_registry
 
     def enqueue(self, iterations: int, data: bytes):
         return self._queue.enqueue(work, iterations=iterations, buffer=data, result_ttl=-1)
@@ -32,3 +32,12 @@ class QueueManager:
                         })
 
         return results
+
+    def isEmpty(self):
+        return self._queue.is_empty()
+
+    def getSize(self):
+        return len(self._queue.jobs)
+
+    def getNumOfWorkers(self):
+        return Worker.count(queue=self._queue)
